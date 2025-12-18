de2b6a74fb7eb971353adfe93855a85df9e3171d
Ismail Messaoudi <ismail.messaoudi@mni.thm.de>
2025-12-18 15:41:09 +0100

    Fix: BackendApplocation.java

--- BACKEND/src/main/java/com/mycargonaut/backend/BackendApplication.java
+++ BACKEND/src/main/java/com/mycargonaut/backend/BackendApplication.java
@@ -1,23 +1,11 @@
 package com.mycargonaut.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles; // <--- WICHTIG
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Wir sagen Spring: "Tu so, als wären wir im 'test'-Profil (das leer ist)"
-// Damit wird 'dev' und damit PostgreSQL ignoriert.
-@ActiveProfiles("test")
-@SpringBootTest(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
-    "spring.datasource.driverClassName=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=password",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-    "spring.jpa.hibernate.ddl-auto=create-drop"
-})
-class BackendApplicationTests {
-
-    @Test
-    void contextLoads() {
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
     }
 }
