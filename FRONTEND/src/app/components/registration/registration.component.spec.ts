eb740c2f48bf73fa955d5179976b77487c23bd30
Mohamed Elsayed <132348752+MohSay890@users.noreply.github.com>
2025-12-25 22:49:28 +0100

    Import ActivatedRoute and of in registration.spec.ts

--- FRONTEND/src/app/components/registration/registration.component.spec.ts
+++ FRONTEND/src/app/components/registration/registration.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegistrationComponent } from './registration.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
