cc63455a2049ab58377e251c0e0dde599e88d114
Mohamed Elsayed <132348752+MohSay890@users.noreply.github.com>
2025-12-25 22:50:05 +0100

    Import ActivatedRoute and of in app.component.spec.ts

--- FRONTEND/src/app/app.component.spec.ts
+++ FRONTEND/src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
